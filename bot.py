e3aa3aa3ca747f42a5a2d8de7fc2cf73364d9d94
George-Kaloudis <georgekal123@gmail.com>
2018-12-09 01:02:36 +0200

    Changed something.

--- bot.py
+++ bot.py
@@ -1,5 +1,8 @@
 import twitter, os, time
 
+"""
+Desktop
+========
 my_tok = 'token.tk'
 cons_key = 'conk.tk'
 if not os.path.isfile(my_tok):
@@ -8,10 +11,26 @@ if not os.path.isfile(my_tok):
 tok, tok_sec = twitter.read_token_file(my_tok)
 ckey, ckey_sec = twitter.read_token_file(cons_key)
 
+
+"""
+
+
+
+"""
+Heroku
+=======
+"""
+tok = os.environ['TOK']
+tok_sec = os.environ['TOK_SEC']
+ckey = os.environ['CKEY']
+ckey_sec = os.environ['CKEY_SEC']
+
+
 api = twitter.Twitter(auth=twitter.OAuth(tok,
                       tok_sec,
                       ckey,
                       ckey_sec))
+
 
 #api.statuses.update(status="Test")
 
